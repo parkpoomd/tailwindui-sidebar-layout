8fe42002c3836ba21191d589ada66e973cc24520
parkpoomd <parkpoom.d@gmail.com>
2020-12-17 16:15:25 +0700

    toolbar and main

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex bg-gray-100">
+  <div class="h-screen flex overflow-hidden bg-gray-100">
     <!-- Off-canvas menu for mobile -->
     <transition leave-active-class="duration-300">
       <div v-show="sidebarOpen" class="md:hidden">
@@ -9,10 +9,10 @@
             -->
           <transition
             enter-active-class="transition-opacity ease-linear duration-300"
-            enter-class="opacity-0"
+            enter-from-class="opacity-0"
             enter-to-class="opacity-100"
             leave-active-class="transition-opacity ease-linear duration-300"
-            leave-class="opacity-100"
+            leave-from-class="opacity-100"
             leave-to-class="opacity-0"
           >
             <div v-show="sidebarOpen" class="fixed inset-0">
@@ -24,10 +24,10 @@
             -->
           <transition
             enter-active-class="transition ease-in-out duration-300 transform"
-            enter-class="-translate-x-full"
+            enter-from-class="-translate-x-full"
             enter-to-class="translate-x-0"
             leave-active-class="transition ease-in-out duration-300 transform"
-            leave-class="translate-x-0"
+            leave-from-class="translate-x-0"
             leave-to-class="-translate-x-full"
           >
             <div
@@ -361,7 +361,158 @@
         </div>
       </div>
     </div>
-    <div>main</div>
+    <div class="flex flex-col w-0 flex-1 overflow-hidden">
+      <div class="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
+        <button
+          @click="sidebarOpen = true"
+          class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:bg-gray-100 focus:text-gray-600 md:hidden"
+          aria-label="Open sidebar"
+        >
+          <svg
+            class="h-6 w-6"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
+          </svg>
+        </button>
+        <div class="flex-1 px-4 flex justify-between">
+          <div class="flex-1 flex">
+            <form class="w-full flex md:ml-0">
+              <label for="search_field" class="sr-only">Search</label>
+              <div
+                class="relative w-full text-gray-400 focus-within:text-gray-600"
+              >
+                <div
+                  class="absolute inset-y-0 left-0 flex items-center pointer-events-none"
+                >
+                  <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  id="search_field"
+                  class="block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm"
+                  placeholder="Search"
+                  type="search"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="ml-4 flex items-center md:ml-6">
+            <button
+              class="p-1 text-gray-400 rounded-full hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring focus:text-gray-500"
+              aria-label="Notifications"
+            >
+              <svg
+                class="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
+
+            <!-- Profile dropdown -->
+            <div>
+              <div>
+                <button
+                  @click="dropdownOpen = !dropdownOpen"
+                  class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:ring"
+                  id="user-menu"
+                  aria-label="User menu"
+                  aria-haspopup="true"
+                >
+                  <img
+                    class="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </button>
+              </div>
+              <!--
+                  Profile dropdown panel, show/hide based on dropdown state.
+                -->
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <div
+                  v-show="dropdownOpen"
+                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
+                >
+                  <div
+                    class="py-1 rounded-md bg-white shadow-sm"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu"
+                  >
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                      role="menuitem"
+                      >Your Profile</a
+                    >
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                      role="menuitem"
+                      >Settings</a
+                    >
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                      role="menuitem"
+                      >Sign out</a
+                    >
+                  </div>
+                </div>
+              </transition>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <main
+        class="flex-1 relative z-0 overflow-y-auto focus:outline-none"
+        tabindex="0"
+      >
+        <div class="pt-2 pb-6 md:py-6">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+          </div>
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+            <!-- Replace with your content -->
+            <div class="py-4">
+              <div
+                class="border-4 border-dashed border-gray-200 rounded-lg h-96"
+              ></div>
+            </div>
+            <!-- /End replace -->
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -370,7 +521,8 @@ export default {
   name: 'App',
   data() {
     return {
-      sidebarOpen: true,
+      sidebarOpen: false,
+      dropdownOpen: false,
     };
   },
 };
